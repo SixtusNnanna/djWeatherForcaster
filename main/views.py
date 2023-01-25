101be76d306bff4b8620282daf04fe683ff33617
sixtus nnanna <omejesixtusnnanna@yahoo.com>
2023-01-25 08:08:55 +0100

    react-integration

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+'''
 # Create your views here.
+def homeView(request):
+    if request.method == 'POST':
+        city = request.POST["city"]
+
+        print(city)
+        return HttpResponseRedirect(reverse("/"))
+
+'''
--- weather_project/settings.py
+++ weather_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "weather_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'react-weather/build')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -122,3 +122,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'react-weather/build/static')
+]
